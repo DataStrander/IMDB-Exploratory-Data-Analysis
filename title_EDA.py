73698e4a8aad4f66bb7bac4e8618537fee42f101
MatteoGevi <38760501+DataStrander@users.noreply.github.com>
2021-09-05 14:01:26 +0200

    Data Manipulation

--- title_EDA.py
+++ title_EDA.py
@@ -12,19 +12,31 @@
 #     name: python3
 # ---
 
+from imdb import IMDb
 import os
 import warnings
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as pt
+import matplotlib.pyplot as plt
+import plotly
+import plotly.graph_objects as go
 
 imdb = pd.read_csv('imdb_df.csv')
 
 imdb.isnull().sum(axis = 0)
 
+imdb = imdb.drop(columns=['primaryTitle','Unnamed: 0','knownForTitles','directors','writers','isAdult','primaryProfession'])
+imdb = imdb.rename(columns={'titleType':'type','originalTitle':'title','primaryName':'director'})
 imdb.head()
 
-imdb = imdb.drop(columns=['primaryTitle','Unnamed: 0','knownForTitles','directors','writers','isAdult','primaryProfession'])
-imdb = imdb.rename(columns={'titleType':'type','originalTitle','title',})
+# +
+'''Data Cleansing'''
+
+print(imdb['type'].unique())
+
+# -
+
+fig = go.Figure(data=go.Scatter(x=imdb['averageRating'], y=imdb['startYear']))
+fig.show()
 
 
